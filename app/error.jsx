f901551c73fb9fd2f83535f522c91d5e72e26c24
Vladimir Nikodijevic <vladimir.nikodijevic@etondigital.com>
2023-06-02 08:37:14 +0200

    use client directive

---
+++ app/error.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Error = () => {
+  return (
+    <div>This page catch errors happend in client components and represent Error Boundary</div>
+  )
+}
+
+export default Error
---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+"use client"
+const Loading = () => {
+  return (
+    <div>
+      This page represent Suspense components that works with nextJS
+    </div>
+  );
+}
+
+export default Loading
